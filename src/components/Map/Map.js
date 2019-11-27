87c1b85b54b31b6df1c757f05a43981a54a7d55a
jundymek <jundymek@gmail.com>
2019-11-28 00:58:41 +0100

    feat: populate map with markers

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import AddNewLocationForm from "../AddNewLocationForm/AddNewLocationForm";
-import LocationsTable from '../LocationsTable/LocationsTable';
+import LocationsTable from "../LocationsTable/LocationsTable";
+import { connect } from "react-redux";
 
-function MyMap() {
+function MyMap(props) {
   const [currentPos, setCurrentPos] = useState([52.237049, 21.017532]);
   const [address, setAddress] = useState(null);
   const [locationName, setLocationName] = useState("");
@@ -15,11 +16,17 @@ function MyMap() {
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={currentPos} draggable={false}>
-          <Popup>
-            {address ? address : ""} <br /> {locationName ? locationName : ""}.
-          </Popup>
-        </Marker>
+        {props.locations.length
+          ? props.locations.map((location, index) => {
+              return (
+                <Marker key={index} position={[location.latitude, location.longitude]} draggable={false}>
+                  <Popup>
+                    {location.address} <br /> {location.name}.
+                  </Popup>
+                </Marker>
+              );
+            })
+          : null}
       </Map>
       <AddNewLocationForm newMarker={setCurrentPos} setMarkerAddress={setAddress} setLocationName={setLocationName} />
       <LocationsTable />
@@ -27,4 +34,8 @@ function MyMap() {
   );
 }
 
-export default MyMap;
+const mapStateToProps = state => ({
+  locations: state.locations.locations
+});
+
+export default connect(mapStateToProps)(MyMap);
