584df96806065b420f4526b818e5a83ce41055ae
jundymek <jundymek@gmail.com>
2020-01-01 23:52:08 +0100

    wip: add PWA support

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./store/reducer";
 import thunk from "redux-thunk";
+import * as serviceWorker from './serviceWorker';
 require('dotenv').config()
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -28,4 +29,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.register();
+serviceWorker.register();
