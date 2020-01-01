ae56f2712763b72da7a2289594af89339ecf4bb9
jundymek <jundymek@gmail.com>
2020-01-02 00:19:21 +0100

    feat: completed  PWA support - icons and favicon

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducer,
   composeEnhancer(applyMiddleware(thunk))
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
