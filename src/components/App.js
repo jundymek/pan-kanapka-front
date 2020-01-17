5abe0fe2ac6e16f4e6e0de4bb3ca133b66557336
jundymek <jundymek@gmail.com>
2020-01-17 22:12:43 +0100

    style: loading spinner

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ function App(props) {
     <div className="main">
       <Header />
       <div className="App App--js">
-        <Suspense fallback={<div>Wczytywanie...</div>}>
+        <Suspense fallback={<div class="loader">Wczytywanie...</div>}>
           <MyMap />
           <LocationsCardsManager />
         </Suspense>
